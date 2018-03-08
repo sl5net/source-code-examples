28b648f175750f455c43a0060e9370344d9243f9
T540P-SL5NET\Administrator <sl5softwarelab@gmail.com>
2018-03-08 15:28:10 +0100

    no message

---
+++ named parameters function call/php.php
@@ -0,0 +1,3 @@
+// from https://philsturgeon.uk/php/2013/09/01/named-parameters-in-php/
+
+$api->getFriends(['screen_name' => 'phpdrama', 'include_user_entities' => true]);
---
+++ named parameters function call/python.py
@@ -0,0 +1,3 @@
+// from https://philsturgeon.uk/php/2013/09/01/named-parameters-in-php/
+
+api.get_friends(screen_name="phpdrama", include_user_entities=true)
