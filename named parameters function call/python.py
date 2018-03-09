9305caff467dac961b2cbda01bdf0789a9084e2c
T540P-SL5NET\Administrator <sl5softwarelab@gmail.com>
2018-03-09 18:55:57 +0100

    no message

--- named parameters function call/python.py
+++ named parameters function call/python.py
@@ -1,3 +1,11 @@
 // from https://philsturgeon.uk/php/2013/09/01/named-parameters-in-php/
 
 api.get_friends(screen_name="phpdrama", include_user_entities=true)
+
+// aus https://en.wikipedia.org/wiki/Named_parameter#Overview
+window.addNewControl(title="Title",
+                     xPosition=20,
+                     yPosition=50,
+                     width=100,
+                     height=50,
+                     drawingNow=true)
