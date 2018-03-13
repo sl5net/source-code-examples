044c5a5e9fe4dad6860a2f7ceeacb14b93911975
T540P-SL5NET\Administrator <sl5softwarelab@gmail.com>
2018-03-13 23:06:16 +0100

    no message

--- named parameters function call/php.php
+++ named parameters function call/php.php
@@ -1,3 +1,20 @@
 // from https://philsturgeon.uk/php/2013/09/01/named-parameters-in-php/
-
-$api->getFriends(['screen_name' => 'phpdrama', 'include_user_entities' => true]);
+<?php
+function getFriends($args) {
+    $args += [ # set default values
+    	'user_id' => null,
+    	'screen_name' => null,
+    	'cursor' => -1,
+    	'skip_status' => false,
+    	'include_user_entities' => false,
+    ];
+    extract($args);
+    echo "<pre>
+        user_id = $user_id,
+    	screen_name = $screen_name,
+    	cursor = $cursor,
+    	skip_status=$skip_status = false,
+    	include_user_entities = $include_user_entities,
+\n";
+}
+getFriends(['screen_name' => 'phpdrama', 'include_user_entities' => true]);
